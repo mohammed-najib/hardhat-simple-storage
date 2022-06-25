0816d91503a85c28f6211a833ac8c40ae025d5e2
mohammed-najib <mohammed.njeeb@gmail.com>
2022-06-25 20:21:04 +0300

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Hardhat Simple Storage
+
+This project is built using both javascript and typscript
+
+Commands I used in this project:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
+```shell
+<!-- Added by me -->
+<!-- commanline commands -->
+yarn hardhat test
+yarn hardhat test --grep store <!-- to invoke test with store word in it -->
+yarn add --dev hardhat-gas-reporter
+yarn add --dev solidity-coverage
+yarn hardhat coverage <!-- how much of the code is covered with testing -->
+yarn add --dev @typechain/ethers-v5 @typechain/hardhat @types/chai @types/node @types/mocha ts-node typechain typescript <!-- to convert project to use typescript -->
+```
+
+# Environment Variables To Set
+RINKEBY_RPC_URL,
+PRIVATE_KEY,
+EHERSCAN_API_KEY,
+COINMARKETCAP_API_KEY,
---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const REINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const EHERSCAN_API_KEY = process.env.EHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: REINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.8",
+    etherscan: {
+        apiKey: EHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+}
